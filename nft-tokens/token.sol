8714960ac295117aeda5a4e3a5eaff541cd354fc
AntonGulak <antongulak7@gmail.com>
2021-10-23 12:25:36 +0900

    beforeCompile

--- nft-tokens/token.sol
+++ nft-tokens/token.sol
@@ -4,68 +4,78 @@ pragma AbiHeader expire;
 // This is class that describes you smart contract.
 contract token {
 
-   // Создаем структуру токена
+   // Структура токена (представим, что это картина, имеющее своё название, автора и год)
     struct Token {
-        string namePicture;
+        string name;
         string nameAuthor;
         uint year;
+    }
+
+    // Структура информации о токене с обратной связкой с массивом
+    struct inf {
+        uint owner;
+        uint price;
+        uint index;
     }
 
 
 // Массив токенов, чтобы там хранить все токены
 Token [] tokenArr;
-// Привязываем токен к конкретному владельцу
- mapping (uint => uint) tokenToOwner;
- 
 
- // Привязываем стоимость к конкретному токену
- mapping (uint => uint) tokenPrice;
+// Привязываем токен к конкретному владельцу
+ mapping (string => inf) tokenToOwner;
+ 
 
 // Создаем новый токен, добавляем его в массив токенов
 // и привязываем к нему адрес  владельца msg.pubkey()
-    function createToken(string namePicture, string nameAuthor, uint year) public {
+    function createToken(string name, string nameAuthor, uint year) public {
         tvm.accept();
-        tokenArr.push(Token(namePicture, nameAuthor, year));
-        uint keyAsLastNum = tokenArr.length - 1;
-        tokenToOwner[keyAsLastNum] = msg.pubkey();
+        require(tokenToOwner.exists(name) == false, 100, "Name already in use. Please choose another one.");
+
+        tokenArr.push(Token(name, nameAuthor, year));
+        tokenToOwner[name] = inf(msg.pubkey(), 0, tokenArr.length - 1);
     }
+
+
 // Получаем адрес владельца по номеру токена
-    function getTokenOwner(uint tokenId) public view returns (uint) {
-        return tokenToOwner[tokenId];
+    function getTokenOwner(string name) public view returns (uint) {
+        return tokenToOwner[name].owner;
     }
-// Получить  информацию о токене по его ID
-    function getTokenInfo(uint tokenId) public view returns (string tokenNamePicture, string tokenNameAuthor, uint tokenYear) {
-        tokenNamePicture = tokenArr[tokenId].namePicture;
-        tokenNameAuthor = tokenArr[tokenId].nameAuthor;
-        tokenYear = tokenArr[tokenId].year;
+
+ // Получаем стоимость  по имени токена
+    function getTokenPrice(string name) public view returns (uint) {
+        return tokenToOwner[name].price;
+    }   
+
+
+// Получить  информацию о токене (автор, год) по его имени
+    function getTokenInfo(string name) public view returns (string tokennameAuthor, uint tokenYear) {
+        tokennameAuthor = tokenArr[tokenToOwner[name].index].nameAuthor;
+        tokenYear = tokenArr[tokenToOwner[name].index].year;
     }
 // Сменить владельца с проверкой совпадения его адреса с адресом меняемого токена
-    function changeOwner(uint tokenId, uint pubkeyOfNewOwner) public {
-        tvm.accept();
-        
-        require(msg.pubkey() == tokenToOwner[tokenId], 101);
-        tokenToOwner[tokenId] = pubkeyOfNewOwner;
+    function changeOwner(string name, uint pubkeyOfNewOwner) public checkOwner(name) {
+
+        tokenToOwner[name].owner = pubkeyOfNewOwner;
     }
 
- // Выставить цену ткоена
-    function changePrice(uint tokenId, uint Price) public {
-        tvm.accept();
+ // Выставить цену токена
+    function changePrice(string name, uint amount) public checkOwner(name) {
 
-        require(msg.pubkey() == tokenToOwner[tokenId], 101);
-        tokenPrice[tokenId] = Price;
+        tokenToOwner[name].price = amount;
     }
    
 
+	modifier checkOwner(string name) {
+		// Check that message was signed with contracts key.
+		require(msg.pubkey() == tokenToOwner[name].owner, 101);
+		tvm.accept();
+		_;
+	}
 
     constructor() public {
-        // Check that contract's public key is set
         require(tvm.pubkey() != 0, 100);
-        // Check that message has signature (msg.pubkey() is not zero) and
-        // message is signed with the owner's private key
         require(msg.pubkey() == tvm.pubkey(), 102);
-        // The current smart contract agrees to buy some gas to finish the
-        // current transaction. This actions required to process external
-        // messages, which bring no value (henceno gas) with themselves.
         tvm.accept();
     }
 
