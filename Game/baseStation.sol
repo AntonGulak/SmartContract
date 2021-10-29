af61eec98466a544a2fedd3ea117f5a33319e946
AntonGulak <55970327+AntonGulak@users.noreply.github.com>
2021-10-29 14:23:56 +0900

    F7

--- Game/baseStation.sol
+++ Game/baseStation.sol
@@ -6,7 +6,21 @@ import "gameObject.sol";
 
 contract baseStation is gameObject {
 
-  address [] unit;
+  address [] private unit;
+  
+  function pushUnit(address dest) public {
+      tvm.accept();
 
+      //Проверка адресса нужна ли?
+      unit.push(dest); }
+
+    function deleteUnit(uint index) public {
+      tvm.accept();
+
+      unit[index] = unit[unit.length - 1];
+      unit.pop;
+    }
+
+    
 
 }
--- Game/militaryUnit.sol
+++ Game/militaryUnit.sol
@@ -5,6 +5,17 @@ pragma AbiHeader expire;
 import "gameObject.sol";
 
 contract militaryUnit is gameObject {
+    address private baseStat;
+
+    constructor() virtual public override{ 
+        require(tvm.pubkey() != 0, 101);
+        tvm.accept();
+
+        baseStat = msg.sender;
+
+    }
+
+    
 
 
 }
