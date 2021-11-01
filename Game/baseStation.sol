f83dde48888a92530b439c187ed98ee551254b31
AntonGulak <antongulak7@gmail.com>
2021-11-01 10:33:47 +0900

    isBetter

--- Game/baseStation.sol
+++ Game/baseStation.sol
@@ -47,16 +47,31 @@ contract baseStation is gameObject, AdittionInterface {
 
     }
     
-    function sendAllValueAndDestroyed()  public override checkOwner() {
-      
-       for (uint i = 0; i < unit.length; i++) {
 
-         //удаление юнита по запросу из станции
-         DeleteUnit un = DeleteUnit(unit[i]);
-         un.deleteUnit(attacker);
-       }
 
-        attacker.transfer(1, true, 128 + 32);
+    function toAttack(int value)  external override {
+        tvm.accept();
+
+        attacker = msg.sender;
+        int damage;
+
+        if (defend >= value ) {
+            damage = 0;
+        }
+        else {
+            damage = value - defend;
+        } 
+
+        HP = HP - damage;
+
+        if (HP <= 0) {
+            for (uint i = 0; i < unit.length; i++) {
+              //удаление юнита по запросу из станции
+              DeleteUnit un = DeleteUnit(unit[i]);
+              un.deleteUnit(attacker); }
+              
+            attacker.transfer(1, true, 128 + 32);
+        }
     }
 
 }
