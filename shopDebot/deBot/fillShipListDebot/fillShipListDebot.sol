6183da61c1b11c07ba8f29f3f6b4a5f2d317210e
AntonGulak <antongulak7@gmail.com>
2021-11-09 22:26:22 +0900

    newSomeFix

--- shopDebot/deBot/fillShipListDebot/fillShipListDebot.sol
+++ shopDebot/deBot/fillShipListDebot/fillShipListDebot.sol
@@ -6,110 +6,54 @@ pragma AbiHeader pubkey;
 
 import "../../abstractClasses/initializationDebot.sol";
 
-contract fillShipLIstDebot is initializationDebot {
+import "../../abstractClasses/menuFunctions/create.sol";
+import "../../abstractClasses/menuFunctions/toBuy.sol";
+import "../../abstractClasses/menuFunctions/del.sol";
 
+
+
+
+contract fillShipLIstDebot is initializationDebot, create, del {
 
     function _menu()  internal override {
         string sep = '----------------------------------------';
-        Menu.select(statToString(),
+
+        if (m_stat.completeCount + m_stat.incompleteCount > 0) {
+             Menu.select(statToString(),
          sep, 
          [
-            MenuItem("Add new product", "", tvm.functionId(createPurchases)),
             MenuItem("Show shopList", "", tvm.functionId(getPurchases)),
+            MenuItem("Add new product", "", tvm.functionId(createPurchase)),
             MenuItem("Delete product", "", tvm.functionId(deletePurchase))
+        ]);
+        } else {
+             Menu.select(statToString(),
+         sep, 
+         [
+            MenuItem("Add new product", "", tvm.functionId(createPurchase))
 
         ]);
+        }
+
         
     } //end menu
 
-    function createPurchases(uint32 index) public {
+
+    function createPurchase(uint32 index)  public  {
         index = index;
-        Terminal.input(tvm.functionId(getAmountPurchase), "Enter product name:", false);
+
+        createPurchase_(m_address);
     }
 
-    function getAmountPurchase(string value) public {
-        inputPur.title = value;
-        Terminal.input(tvm.functionId(createPurchase_), "Enter amount:", false);
-    }
-
-    function createPurchase_(string value) public {
-        (uint256 amount, ) = stoi(value);
-        inputPur.amount = uint32(amount);
-
-        optional(uint256) pubkey = 0;
-        ShopInter(m_address).createPurchase{
-                abiVer: 2,
-                extMsg: true,
-                sign: true,
-                pubkey: pubkey,
-                time: uint64(now),
-                expire: 0,
-                callbackId: tvm.functionId(onSuccess),
-                onErrorId: tvm.functionId(onError)
-     
-            }(inputPur.title, inputPur.amount);
-    }
     
-     function getPurchases(uint32 index) public view {
+    function deletePurchase(uint32 index)  public  {
         index = index;
-        optional(uint256) none;
 
-        ShopInter(m_address).getPurchases{
-            abiVer: 2,
-            extMsg: true,
-            sign: false,
-            pubkey: none,
-            time: uint64(now),
-            expire: 0,
-            callbackId: tvm.functionId(getPurchases_),
-            onErrorId: 0
-        }();
-     }
-
-    function getPurchases_ (ShopInter.Purchase[] purchases) public {
-        uint32 i;
-        if (purchases.length > 0 ) {
-            Terminal.print(0, "Your shopList:");
-            for (i = 0; i < purchases.length; i++) {
-                ShopInter.Purchase purchas = purchases[i];
-                string completed;
-
-                if (purchas.isSoldOut) {
-                    completed = '✓';
-                } else {
-                    completed = ' ';
-                }
-                Terminal.print(0, format("id: {} | {}  \"{}\"  at {}, amount: {}, the total cost:  {}", purchas.id, completed, purchas.title, purchas.createdAt, purchas.amount, purchas.cost));
-            }
-        } else {
-            Terminal.print(0, "Your shopList is empty");
-        }
-        _menu();
+        deletePurchase_(m_address);
     }
+         
 
-    function deletePurchase(uint32 index) public {
-        index = index;
-        if (m_stat.completeCount + m_stat.incompleteCount > 0) {
-            Terminal.input(tvm.functionId(deletePurchase_), "Enter task number:", false);
-        } else {
-            Terminal.print(0, "Sorry, you have no tasks to delete");
-            _menu();
-        }
-    }
 
-    function deletePurchase_(string value) public view {
-        (uint256 num,) = stoi(value);
-        optional(uint256) pubkey = 0;
-        ShopInter(m_address).deletePurchase{
-                abiVer: 2,
-                extMsg: true,
-                sign: true,
-                pubkey: pubkey,
-                time: uint64(now),
-                expire: 0,
-                callbackId: tvm.functionId(onSuccess),
-                onErrorId: tvm.functionId(onError)
-            }(uint32(num));
-    }
+    
 
 } //end contract
--- shopDebot/deBot/shoppingDebot/shoppingDebot.sol
+++ shopDebot/deBot/shoppingDebot/shoppingDebot.sol
@@ -6,121 +6,44 @@ pragma AbiHeader pubkey;
 
 import "../../abstractClasses/initializationDebot.sol";
 
-contract shopping is initializationDebot {
+import "../../abstractClasses/menuFunctions/create.sol";
+import "../../abstractClasses/menuFunctions/toBuy.sol";
+import "../../abstractClasses/menuFunctions/del.sol";
+
+
+
+
+contract summaryDebot is initializationDebot, create, toBuy, del {
 
     function _menu()  internal override {
         string sep = '----------------------------------------';
-        Menu.select(statToString(),
+
+        if (m_stat.completeCount + m_stat.incompleteCount > 0) {
+             Menu.select(statToString(),
          sep, 
          [
-            MenuItem("Update purchase status ", "", tvm.functionId(toBuy)),
             MenuItem("Show shopList", "", tvm.functionId(getPurchases)),
+            MenuItem("Update purchase status ", "", tvm.functionId(toBuy)),
             MenuItem("Delete product", "", tvm.functionId(deletePurchase))
-
         ]);
+        } else {
+              Terminal.input(0, "Enter address contract, where you save shopList:", false);
+        }
+
         
     } //end menu
 
-    function toBuy(uint32 index) public {
+
+    function toBuy(uint32 index)  public  {
         index = index;
-        if (m_stat.completeCount + m_stat.incompleteCount > 0) {
-            Terminal.input(tvm.functionId(toBuyGetFlag), "Enter product number:", false);
-        } else {
-            Terminal.print(0, "Sorry, you have product to update");
-            _menu();
-        }
-    }
 
-    function toBuyGetFlag(string value) public {
-        (uint256 num,) = stoi(value);
-        inputPur.id = uint32(num);
-        ConfirmInput.get(tvm.functionId(toBuyGetCost),"Is this purchase completed?");
+        toBuy_(m_address);
     }
-
-    function toBuyGetCost(bool value) public {
-        inputPur.isSoldOut = value;
- 
-        Terminal.input(tvm.functionId(updatePurchase__), "Enter total purchase price:", false);
-    } 
     
-
-      function updatePurchase__(string value) public {
-        optional(uint256) pubkey = 0;
-        (uint256 num,) = stoi(value);
-        inputPur.cost = uint(num);
-
-        ShopInter(m_address).updatePurchase{
-                abiVer: 2,
-                extMsg: true,
-                sign: true,
-                pubkey: pubkey,
-                time: uint64(now),
-                expire: 0,
-                callbackId: tvm.functionId(onSuccess),
-                onErrorId: tvm.functionId(onError)
-            }(inputPur.id, inputPur.isSoldOut, inputPur.cost);
-    }  
-    
-     function getPurchases(uint32 index) public view {
+    function deletePurchase(uint32 index)  public  {
         index = index;
-        optional(uint256) none;
 
-        ShopInter(m_address).getPurchases{
-            abiVer: 2,
-            extMsg: true,
-            sign: false,
-            pubkey: none,
-            time: uint64(now),
-            expire: 0,
-            callbackId: tvm.functionId(getPurchases_),
-            onErrorId: 0
-        }();
-     }
-
-    function getPurchases_ (ShopInter.Purchase[] purchases) public {
-        uint32 i;
-        if (purchases.length > 0 ) {
-            Terminal.print(0, "Your shopList:");
-            for (i = 0; i < purchases.length; i++) {
-                ShopInter.Purchase purchas = purchases[i];
-                string completed;
-
-                if (purchas.isSoldOut) {
-                    completed = '✓';
-                } else {
-                    completed = ' ';
-                }
-                Terminal.print(0, format("id: {} | {}  \"{}\"  at {}, amount: {}, the total cost:  {}", purchas.id, completed, purchas.title, purchas.createdAt, purchas.amount, purchas.cost));
-            }
-        } else {
-            Terminal.print(0, "Your shopList is empty");
-        }
-        _menu();
+        deletePurchase_(m_address);
     }
-
-    function deletePurchase(uint32 index) public {
-        index = index;
-        if (m_stat.completeCount + m_stat.incompleteCount > 0) {
-            Terminal.input(tvm.functionId(deletePurchase_), "Enter task number:", false);
-        } else {
-            Terminal.print(0, "Sorry, you have no tasks to delete");
-            _menu();
-        }
-    }
-
-    function deletePurchase_(string value) public view {
-        (uint256 num,) = stoi(value);
-        optional(uint256) pubkey = 0;
-        ShopInter(m_address).deletePurchase{
-                abiVer: 2,
-                extMsg: true,
-                sign: true,
-                pubkey: pubkey,
-                time: uint64(now),
-                expire: 0,
-                callbackId: tvm.functionId(onSuccess),
-                onErrorId: tvm.functionId(onError)
-            }(uint32(num));
-    }
-
+         
 } //end contract
