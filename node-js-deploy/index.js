1056a01186c1ec53fd71581e52e5449a59559a21
AntonGulak <antongulak7@gmail.com>
2021-11-18 22:57:39 +0900

    add_dabi

--- node-js-deploy/index.js
+++ node-js-deploy/index.js
@@ -1,7 +1,7 @@
 const {TonClient, signerKeys} = require("@tonclient/core");
 const {libNode} = require("@tonclient/lib-node");
 const { Account } = require("@tonclient/appkit");
-const { consoleTerminal, runCommand } = require("tondev");
+const { consoleTerminal, runCommand} = require("tondev");
 const path = require("path");
 const fs = require('fs');
 const crypto = require('crypto');
@@ -16,15 +16,23 @@ async function main(client) {
 }
 
 (async () => {
-    const endpoint =  "http://localhost";
-    const client = new TonClient({ network: { endpoints: [endpoint] } });
+    const client = new TonClient({
+        network: {
+            endpoints: ["http://localhost"]
+        }
+    });
     try {
+        console.log("Hello localhost TON!");
         await main(client);
-    } catch (err) {
-        console.error(err);
-    } finally {
-        client.close();
+        process.exit(0);
+    } catch (error) {
+        if (error.code === 504) {
+            console.error(`Network is inaccessible.`);
+        } else {
+            console.error(error);
+        }
     }
+    client.close();
 })();
 
 async function main(client) {
@@ -32,7 +40,7 @@ async function main(client) {
     
     const hash = crypto.createHash('md5').update(solFile).digest('hex');
 
-    fs.appendFile(hash + ".sol", solFile, function (err) {
+    fs.writeFile(hash + ".sol", solFile, function (err) {
         if (err) return console.log(err);
         console.log('Create!');
      });
@@ -67,4 +75,14 @@ async function main(client) {
     //Деплоим
     await acc.deploy({ useGiver: true });
     console.log(`Hello contract was deployed at address: ${address}`);
+
+
+    var dabi = {
+        dabi: Buffer.from(JSON.stringify(abi)).toString('base64'),
+    };
+
+
+
+    console.log(dabi);
+
 }
